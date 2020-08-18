fc18b9066268e5e32c0bdb9b1eee387cee772552
Pasi Raumonen <raumonen@mat-mbp-22.intra.tut.fi>
2020-08-18 15:30:05 +0300

    Small changes in PLOT_SCATTER

--- src/plotting/plot_scatter.m
+++ src/plotting/plot_scatter.m
@@ -9,10 +9,20 @@ function plot_scatter(P,C,fig,ms)
 % fig   figure number
 % ms    marker size
 
+S = normalize(C);
 figure(fig)
 if size(P,2) == 3
     scatter3(P(:,1),P(:,2),P(:,3),ms*ones(size(P,1),1),C,'filled')
+    %scatter3(P(:,1),P(:,2),P(:,3),ms*S.*ones(size(P,1),1),C,'filled')
 elseif size(P,2) == 2
-    scatter(P(:,1),P(:,2),ms*ones(size(P,1),1),C,'filled')
+    scatter(P(:,1),P(:,2),ms*S.*ones(size(P,1),1),C,'filled')
 end
 axis equal
+if size(C,2) == 1
+    colormap(jet(25))
+    %caxis([0 max(C)])
+    if min(C) < max(C)
+    caxis([min(C) max(C)])
+    end
+    colorbar
+end
