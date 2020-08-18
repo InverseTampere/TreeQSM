e0227488ffc74f0ca5e810d0084c3b76d9ea52d2
Pasi Raumonen <raumonen@mat-mbp-22.intra.tut.fi>
2020-08-18 14:48:29 +0300

    Changes in commenting
    Changed commenting. Removed the input parameters lcyl and FilRad.

--- src/create_input.m
+++ src/create_input.m
@@ -1,69 +1,80 @@
-% This file is part of TREEQSM.
-% 
-% TREEQSM is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% TREEQSM is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with TREEQSM.  If not, see <http://www.gnu.org/licenses/>.
 
-
-% creates input parameter structure array needed to run TreeQSM function
+% Creates input parameter structure array needed to run TreeQSM function
 
 clear inputs
 
 %% QSM reconstruction parameters
-% The following parameters can be varied and should be optimised (they 
-% can have multiple values given as vectors, e.g. [4 6]):
-inputs.PatchDiam1 = [0.1 0.15]; % Patch size of the first uniform-size cover
-inputs.PatchDiam2Min = [0.02 0.03]; % Minimum patch size of the cover sets in the second cover
-inputs.PatchDiam2Max = [0.06 0.08]; % Maximum cover set size in the stem's base in the second cover
-inputs.lcyl = [3 5]; % Relative (length/radius) length of the cylinders
-inputs.FilRad = 3; % Relative radius for outlier point filtering
+% THE FIVE INPUT PARAMETERS TO BE OPTIMIZED.
+% These CAN BE VARIED AND SHOULD BE OPTIMIZED 
+% (they can have multiple values given as vectors, e.g. [0.01 0.02]).
+% Patch size of the first uniform-size cover:
+inputs.PatchDiam1 = [0.08 0.12]; 
+% Minimum patch size of the cover sets in the second cover:
+inputs.PatchDiam2Min = [0.02 0.03]; 
+% Maximum cover set size in the stem's base in the second cover:
+inputs.PatchDiam2Max = [0.07 0.1]; 
 
-% The following parameters can be varied and but usually can be kept as
-% shown (i.e. little bigger than PatchDiam parameters):
-inputs.BallRad1 = inputs.PatchDiam1+0.02; % Ball radius in the first uniform-size cover generation
-inputs.BallRad2 = inputs.PatchDiam2Max+0.01; % Maximum ball radius in the second cover generation
+% ADDITIONAL PATCH GENERATION PARAMETERS.
+% The following parameters CAN BE VARIED BUT CAN BE USUALLY KEPT AS SHOWN 
+% (i.e. little bigger than PatchDiam parameters).
+% Ball radius in the first uniform-size cover generation:
+inputs.BallRad1 = inputs.PatchDiam1+0.015; 
+% Maximum ball radius in the second cover generation:
+inputs.BallRad2 = inputs.PatchDiam2Max+0.01; 
 
-% The following parameters can be usually kept fixed as shown:
-inputs.nmin1 = 3; % Minimum number of points in BallRad1-balls, generally good value is 3
-inputs.nmin2 = 1; % Minimum number of points in BallRad2-balls, generally good value is 1
-inputs.OnlyTree = 1; % If 1, point cloud contains points only from the tree
-inputs.Tria = 1; % If 1, produces a triangulation 
-inputs.Dist = 1; % If 1, computes the point-model distances
+% The following parameters CAN BE USUALLY KEPT FIXED as shown.
+% Minimum number of points in BallRad1-balls, generally good value is 3:
+inputs.nmin1 = 3; 
+% Minimum number of points in BallRad2-balls, generally good value is 1:
+inputs.nmin2 = 1; 
+% Does the point cloud contain points only from the tree (if 1, then yes):
+inputs.OnlyTree = 1; 
+% Produce a triangulation of the stem's bottom part up to the first main
+% branch (if 1, then yes):
+inputs.Tria = 1; 
+% Compute the point-model distances (if 1, then yes):
+inputs.Dist = 1; 
 
-% Different cylinder radius correction options for modifying too large and
-% too small cylinders:
+% RADIUS CORRECTION OPTIONS FOR MODIFYING TOO LARGE AND TOO SMALL CYLINDERS.
+% These parameters CAN BE USUALLY KEPT FIXED as shown.
 % Traditional TreeQSM choices:
-inputs.MinCylRad = 0.0025; % Minimum cylinder radius, used particularly in the taper corrections
-inputs.ParentCor = 1; % Radii in a child branch are always smaller than the radii of the 
-                      % parent cylinder in the parent branch
-inputs.TaperCor = 1; % Use partially linear (stem) and parabola (branches) taper corrections
+% Minimum cylinder radius, used particularly in the taper corrections:
+inputs.MinCylRad = 0.0025; 
+% Radius correction based on radius of the parent. If 1, radii in a branch 
+% are always smaller than the radius of the parent in the parent branch:
+inputs.ParentCor = 1; 
+% Taper correction of radii inside branches. If 1, use partially linear 
+% (stem) and parabola (branches) taper corrections:
+inputs.TaperCor = 1; 
+
 % Growth volume correction approach introduced by Jan Hackenberg, 
 % allometry: GrowthVol = a*Radius^b+c
-inputs.GrowthVolCor = 0; % Use growth volume (GV) correction 
-inputs.GrowthVolFac = 2.5; % fac-parameter of the GV-approach, defines upper and lower bound
-                           % When using GV-approach, consider setting:
-                           % TaperCorr = 0, ParentCorr = 0, MinCylinderRadius = 0.
+inputs.GrowthVolCor = 0; % If 1, use growth volume (GV) correction 
+% fac-parameter of the GV-approach, defines upper and lower bound. When 
+% using GV-approach, consider setting TaperCorr = 0, ParentCorr = 0, 
+% MinCylinderRadius = 0.
+inputs.GrowthVolFac = 2.5; 
 
 %% Other inputs
 % These parameters don't affect the QSM-reconstruction but define what is
 % saved, plotted, and displayed and how the models are named/indexed
-inputs.name = 'pine'; % Name string for saving output files and naming models
-inputs.tree = 1; % Tree index. If modelling multiple trees, then they can be indexed uniquely
-inputs.model = 1; % Model index, can separate models if multiple models with the same inputs
-inputs.savemat = 1; % If 1, saves the output struct QSM as a matlab-file into \result folder 
-                    % If name = 'pine', tree = 2, model = 5, 
-                    % the name of the saved file is 'QSM_pine_t2_m5.mat'
-inputs.savetxt = 1; % If 1, saves the models in .txt-files
-inputs.plot = 1; % If 1, plots the model, the segmentation of the point cloud and distributions
-inputs.disp = 2; % Defines what is displayed during the reconstruction:
-                 % 2 = display all; 1 = display name, parameters and distances;
-                 % 0 = display only the name
+% Name string for saving output files and naming models:
+inputs.name = 'tree'; 
+% Tree index. If modelling multiple trees, then they can be indexed uniquely:
+inputs.tree = 1;
+% Model index, can separate models if multiple models with the same inputs:
+inputs.model = 1; 
+% Save the output struct QSM as a matlab-file into \result folder. 
+% If name = 'pine', tree = 2, model = 5, the name of the saved file is 
+% 'QSM_pine_t2_m5.mat':
+inputs.savemat = 1; 
+% Save the models in .txt-files (check "save_model_text.m"):
+inputs.savetxt = 0; 
+% What are plotted during reconstruction process: 
+% 2 = plots the QSM, the segmentated point cloud and distributions, 
+% 1 = plots the QSM and the segmentated point cloud
+% 0 = plots nothing
+inputs.plot = 2; 
+% What are displayed during the reconstruction: 2 = display all; 
+% 1 = display name, parameters and distances; 0 = display only the name:
+inputs.disp = 2; 
