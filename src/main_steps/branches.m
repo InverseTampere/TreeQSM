d1948da7ed3a4ad7658c61b603e3e4e6e5afb5b0
Pasi Raumonen <raumonen@mat-mbp-22.intra.tut.fi>
2020-08-18 15:06:44 +0300

    Changes in BRANCHES
    Changed the coding to simplify and shorten the code. Added branch area
    and zenith direction as new fields in the branch-structure array.
    Removed the line were 'ChildCyls' and 'CylsInSegment' fields are
    removed from the cylinder-structure array.

--- src/main_steps/branches.m
+++ src/main_steps/branches.m
@@ -19,10 +19,10 @@ function [branch,cylinder] = branches(segment,cylinder)
 % BRANCHES.M        Determines the branching structure and computes branch
 %                       attributes
 %
-% Version 2.00
-% Latest update     16 Aug 2017
+% Version 2.1.0
+% Latest update     25 Jan 2020
 %
-% Copyright (C) 2013-2017 Pasi Raumonen
+% Copyright (C) 2013-2020 Pasi Raumonen
 % ---------------------------------------------------------------------
 
 % Determines the branches (cylinders in a segment define a branch), their order
@@ -40,87 +40,88 @@ function [branch,cylinder] = branches(segment,cylinder)
 %             Branch order, parent, volume, length, angle, height, azimuth and diameter
 % cylinder  Updated cylinder structure array, contains new fields:
 %             Branch of the cylinder, branch order, position inside the branch
+% ---------------------------------------------------------------------
 
-Segs = segment.segments;
-SChi = segment.ChildSegment;
+% Changes from version 2.0.0 to 2.1.0, 25 Jan 2020:
+% 1) Changed the coding to simplify and shorten the code
+% 2) Added branch area and zenith direction as new fields in the 
+%    branch-structure array
+% 3) Removed the line were 'ChildCyls' and'CylsInSegment' fields are
+%    removed from the cylinder-structure array 
+
 Rad = cylinder.radius;
 Len = cylinder.length;
-Sta = cylinder.start;
 Axe = cylinder.axis;
-CPar = cylinder.parent;
-Added = cylinder.added;
-CChi = cylinder.ChildCyls;
-CiS = cylinder.CylsInSegment;
-cylinder = rmfield(cylinder,{'ChildCyls','CylsInSegment'});
 
 %% Branches
 nc = size(Rad,1);  % number of cylinder
-ns = size(Segs,1);  % number of segments
-BOrd = zeros(ns,1); % Branch order of the branch
+ns = size(segment.segments,1);  % number of segments
 CiB = cell(ns,1); % Cylinders in each branch
-BoC = zeros(nc,1); % Branch of cylinder
-BOrdC = zeros(nc,1); % Branch order of the cylinder
-CPiB = zeros(nc,1); % Cylinder's position inside the branch
-C = CiS{1};
+C = cylinder.CylsInSegment{1};
 CiB{1} = C;
-BoC(C) = 1;
-CPiB(C) = (1:length(C))';
-BVol = zeros(ns,1); % branch volume
-BLen = zeros(ns,1); % branch length
-BAng = zeros(ns,1); % branch angle
-BHei = zeros(ns,1); % branch height
-BAzi = zeros(ns,1); % azimuth of branch
-BDia = zeros(ns,1); % branch diameter at the base
-BDia(1) = 2*Rad(1);
-BVol(1) = pi*sum(Len(C).*Rad(C).^2);
-BLen(1) = sum(Len(C));
-
-S = SChi{1};    % segments under inspection
+CData = zeros(nc,3); % cylinder's branch, b-order, position in the branch
+CData(C,2) = 1;
+CData(C,3) = (1:length(C))';
+BData = zeros(ns,9); % branch ord, dia, vol, are, len, ang, hei, azi, zen
+BData(1,2) = 2*Rad(1); % diameter
+BData(1,3) = 1000*pi*sum(Len(C).*Rad(C).^2); % volume
+BData(1,4) = 2*pi*sum(Len(C).*Rad(C)); % area
+BData(1,5) = sum(Len(C)); % length
+BData(1,8) = 180/pi*atan2(Axe(1,2),Axe(1,1)); % azimuth
+BData(1,9) = 180/pi*acos(Axe(1,3)); % zenith
+S = segment.ChildSegment{1};    % segments under inspection
 b = 1;          % branches determined so far
 BO = 0;         % branch order under inspection
 while ~isempty(S)
     BO = BO+1;
     n = length(S);
     for j = 1:n
-        C = CiS{S(j)};
+        C = cylinder.CylsInSegment{S(j)};
         if ~isempty(C)
             b = b+1;
             CiB{b} = C;
-            BOrd(b) = BO;
-            BoC(C) = b;
-            BOrdC(C) = BO;
-            CPiB(C) = (1:length(C))';
-            BVol(b) = pi*sum(Len(C).*Rad(C).^2);
-            BLen(b) = sum(Len(C));
-            BHei(b) = Sta(C(1),3)-Sta(1,3);
-            BAzi(b) = 180/pi*atan2(Axe(C(1),2),Axe(C(1),1));
-            BDia(b) = 2*Rad(C(1));
-            I = Added(C(1));  % if the first cylinder is added to fill gap, 
-                              % use the second cylinder
-            if I
-                FC = C(2);          % first cyl in the branch
-                PC = CPar(C(I));    % parent cylinder of the branch
+            CData(C,1) = BO;
+            CData(C,2) = b;
+            CData(C,3) = (1:length(C))';
+            
+            BData(b,1) = BO; % branch order
+            BData(b,2) = 2*Rad(C(1)); % branch diameter
+            BData(b,3) = 1000*pi*sum(Len(C).*Rad(C).^2); % branch volume
+            BData(b,4) = 2*pi*sum(Len(C).*Rad(C)); % branch area
+            BData(b,5) = sum(Len(C)); % branch length
+            
+            % if the first cylinder is added to fill a gap, then
+            % use the second cylinder:
+            added = cylinder.added(C(1));
+            if added
+                FC = C(2);  % first cyl in the branch
+                PC = cylinder.parent(C(added)); % parent cylinder of the branch
             else
                 FC = C(1);
-                PC = CPar(FC);
+                PC = cylinder.parent(FC);
             end
             if PC > 0
-                BAng(b) = 180/pi*acos(Axe(FC,:)*Axe(PC,:)');
+                BData(b,6) = 180/pi*acos(Axe(FC,:)*Axe(PC,:)'); % branch angle
             end
+            
+            BData(b,7) = cylinder.start(C(1),3)-cylinder.start(1,3); % branch height
+            BData(b,8) = 180/pi*atan2(Axe(C(1),2),Axe(C(1),1)); % branch azimuth
+            BData(b,9) = 180/pi*acos(Axe(C(1),3)); % branch zenith
         end
     end
-    S = vertcat(SChi{S});
+    S = vertcat(segment.ChildSegment{S});
 end
 CiB = CiB(1:b);
+BData = single(BData(1:b,:));
 clear branch
 
 %% Branching structure (topology, parent-child-relation)
-branch.order = uint8(BOrd(1:b));
+branch.order = uint8(BData(:,1));
 BPar = zeros(b,1);
 for i = 1:b
     C = CiB{i};
-    ChildCyls = unique(vertcat(CChi{C}));
-    CB = unique(BoC(ChildCyls));  % Child branches
+    ChildCyls = unique(vertcat(cylinder.ChildCyls{C}));
+    CB = unique(CData(ChildCyls,2));  % Child branches
     BPar(CB) = i; 
 end
 if b <= 2^16
@@ -130,22 +131,24 @@ else
 end
 
 %% Finish the definition of branch
-branch.volume = single(1000*BVol(1:b));  % volumes in liters
-branch.length = single(BLen(1:b));  % lengths in meters
-branch.angle = single(BAng(1:b)); % angles in degrees
-branch.height = single(BHei(1:b)); % heights in meters
-branch.azimuth = single(BAzi(1:b)); % aximuths in angles
-branch.diameter = single(BDia(1:b));  % diameters in meters
+branch.diameter = BData(:,2);   % diameters in meters
+branch.volume = BData(:,3);     % volumes in liters
+branch.area = BData(:,4);       % areas in square meters
+branch.length = BData(:,5);     % lengths in meters
+branch.angle = BData(:,6);      % angles in degrees
+branch.height = BData(:,7);     % heights in meters
+branch.azimuth = BData(:,8);    % azimuth directions in angles
+branch.zenith = BData(:,9);     % zenith directions in angles
 
 %% Add cylinder outputs
 if b <= 2^16
-    cylinder.branch = uint16(BoC);
+    cylinder.branch = uint16(CData(:,2));
 else
-    cylinder.branch = uint32(BoC);
+    cylinder.branch = uint32(CData(:,2));
 end
-cylinder.BranchOrder = uint8(BOrdC);
-if max(CPiB) <= 2^8
-    cylinder.PositionInBranch = uint8(CPiB);
+cylinder.BranchOrder = uint8(CData(:,1));
+if max(CData(:,3)) <= 2^8
+    cylinder.PositionInBranch = uint8(CData(:,3));
 else
-    cylinder.PositionInBranch = uint16(CPiB);
+    cylinder.PositionInBranch = uint16(CData(:,3));
 end
