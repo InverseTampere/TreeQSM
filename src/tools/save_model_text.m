a65ec03ec2646fcc14981e95e632b0c48e579817
Pasi Raumonen <raumonen@mat-mbp-22.intra.tut.fi>
2020-08-18 15:34:03 +0300

    Changes in SAVE_MODEL_TEXT
    Added the new fields of cylinder, branch and treedata structures. Added
    header names to the files. Changed the names of the files to be saved.
    Changed the name of second input from "string" to "savename”. Changed
    the rounding of some parameters and attributes.

--- src/tools/save_model_text.m
+++ src/tools/save_model_text.m
@@ -13,15 +13,34 @@
 % You should have received a copy of the GNU General Public License
 % along with TREEQSM.  If not, see <http://www.gnu.org/licenses/>.
 
-function save_model_text(QSM,string)
+function save_model_text(QSM,savename)
+
+% ---------------------------------------------------------------------
+% SAVE_MODEL_TEXT.M       Saves QSM (cylinder, branch, treedata) into text
+%                           files
+%
+% Version 1.1.0
+% Latest update     17 Aug 2020
+%
+% Copyright (C) 2013-2020 Pasi Raumonen
+% ---------------------------------------------------------------------
 
 % Save the cylinder, branch, and treedata structures in text-formats (.txt) 
-% into /result-folder
-
-% !!! Notice that only part of the treedata is saved in the text-file and
-% the treedata.txt is different than in previous versions.
+% into /result-folder with the input "savename" defining the file names:
+% 'cylinder_',savename,'.txt'
+% 'branch_',savename,'.txt'
+% 'treedata_',savename,'.txt'
+% !!! Notice that only part of the treedata, the single number tree 
+% attributes are saved in the text-file.
 % Every user can change this code easily to define what is saved into 
 % their text-files.
+
+% Changes from version 1.0.0 to 1.1.0, 17 Aug 2020:
+% 1) Added the new fields of cylinder, branch and treedata structures
+% 2) Added header names to the files
+% 3) Changed the names of the files to be saved
+% 4) Changed the name of second input from "string" to "savename"
+% 5) Changed the rounding of some parameters and attributes
 
 cylinder = QSM.cylinder;
 branch = QSM.branch;
@@ -29,81 +48,70 @@ treedata = QSM.treedata;
 
 %% Form cylinder data, branch data and tree data
 % Use less decimals
-Rad = round(10000*cylinder.radius)/10000; % radius in meters
-Len = round(10000*cylinder.length)/10000; % length in meters
-Sta = round(10000*cylinder.start)/10000; % starting point in meters
-Axe = round(10000*cylinder.axis)/10000; % axis in meters
-CPar = single(cylinder.parent);
-CExt = single(cylinder.extension);
-Added = single(cylinder.added);
-Rad0 = round(10000*cylinder.UnmodRadius)/10000;
-B = single(cylinder.branch);
-BO = single(cylinder.BranchOrder);
-PIB = single(cylinder.PositionInBranch);
-CylData = [Rad Len Sta Axe CPar CExt B BO PIB Added Rad0];
+Rad = round(10000*cylinder.radius)/10000; % radius (m)
+Len = round(10000*cylinder.length)/10000; % length (m)
+Sta = round(10000*cylinder.start)/10000; % starting point (m)
+Axe = round(10000*cylinder.axis)/10000; % axis (m)
+CPar = single(cylinder.parent); % parent cylinder
+CExt = single(cylinder.extension); % extension cylinder
+Added = single(cylinder.added); % is cylinder added to fil a gap
+Rad0 = round(10000*cylinder.UnmodRadius)/10000; % unmodified radius (m)
+B = single(cylinder.branch); % branch index of the cylinder
+BO = single(cylinder.BranchOrder); % branch order of the branch
+PIB = single(cylinder.PositionInBranch); % position of the cyl. in the branch
+Mad = single(round(10000*cylinder.mad)/10000); % mean abso. distance (m)
+SC = single(round(10000*cylinder.SurfCov)/10000); % surface coverage
+CylData = [Rad Len Sta Axe CPar CExt B BO PIB Mad SC Added Rad0];
+NamesC = ['radius (m)',"length (m)","start_point","axis_direction",...
+  "parent","extension","branch","branch_order","position_in_branch",...
+  "mad","SurfCov","added","UnmodRadius (m)"];
 
-BOrd = single(branch.order);
-BPar = single(branch.parent);
-BVol = round(10000*branch.volume)/10000; % volume in litres
-BLen = round(10000*branch.length)/10000; % length in meters
-BAng = branch.angle; % angle in degrees
-BHei = branch.height; % height in metres
-BAzi = branch.azimuth; % azimuth in degrees
-BDia = round(10000*branch.diameter)/10000; % diameter in meters
-BranchData = [BOrd BPar BVol BLen BAng BHei BAzi BDia];
+BOrd = single(branch.order); % branch order
+BPar = single(branch.parent); % parent branch
+BDia = round(10000*branch.diameter)/10000; % diameter (m)
+BVol = round(10000*branch.volume)/10000; % volume (L)
+BAre = round(10000*branch.area)/10000; % area (m^2)
+BLen = round(1000*branch.length)/1000; % length (m)
+BAng = round(10*branch.angle)/10; % angle (deg)
+BHei = round(1000*branch.height)/1000; % height (m)
+BAzi = round(10*branch.azimuth)/10; % azimuth (deg)
+BZen = round(10*branch.zenith)/10; % zenith (deg)
+BranchData = [BOrd BPar BDia BVol BAre BLen BHei BAng BAzi BZen];
+NamesB = ["order","parent","diameter (m)","volume (L)","area (m^2)",...
+  "length (m)","height (m)","angle (deg)","azimuth (deg)","zenith (deg)"];
 
+% Extract the field names of treedata
 Names = fieldnames(treedata);
 n = 1;
-while ~strcmp(Names{n}(1:3),'DBH')
+while ~strcmp(Names{n},'location')
     n = n+1;
 end
-if size(Names,1) <= 18
-    % No triangulation model
-    n = n+1;
-else
-    % Triangulation model
-    n = n+6;
-end
+n = n-1;
+Names = Names(1:n);
+
 TreeData = zeros(n,1); 
-% TreeData contains TotalVolume, TrunkVolume, BranchVolume, TreeHeight, 
-% TrunkLength, BranchLength, NumberBranches, MaxBranchOrder, TotalArea, DBHqsm, DBHcyl.
-% If triangulation model, then TreeData contains the following additional
-% data: DBHtri, TriaTrunkVolume, MixTrunkVolume, MixTotalVolume, TriaTrunkLength 
+% TreeData contains TotalVolume, TrunkVolume, BranchVolume, etc
 for i = 1:n
     TreeData(i) = treedata.(Names{i,:});
 end
-% % Add branch order data up to sixth order as was used in some earlier versions
-% % in the treedata.txt
-% for i = 1:min(6,treedata.MaxBranchOrder)
-%     TreeData(n+i) = treedata.NumberBranchOrder(i);
-% end
-% n = n+6;
-% for i = 1:min(6,treedata.MaxBranchOrder)
-%     TreeData(n+i) = treedata.VolumeBranchOrder(i);
-% end
-% n = n+6;
-% for i = 1:min(6,treedata.MaxBranchOrder)
-%     TreeData(n+i) = treedata.LengthBranchOrder(i);
-% end
-% if treedata.MaxBranchOrder < 6
-%     n = n+6;
-%     TreeData(n) = 0;
-% end
-
 TreeData = change_precision(TreeData); % use less decimals
+NamesD = string(Names);
 
 %% Save the data as text-files
-str = ['results/cyl_data_',string,'.txt'];
+str = ['results/cylinder_',savename,'.txt'];
 fid = fopen(str, 'wt');
+fprintf(fid, [repmat('%s\t', 1, size(NamesC,2)-1) '%s\n'], NamesC.');
 fprintf(fid, [repmat('%g\t', 1, size(CylData,2)-1) '%g\n'], CylData.');
 fclose(fid);
 
-str = ['results/branch_data_',string,'.txt'];
+str = ['results/branch_',savename,'.txt'];
 fid = fopen(str, 'wt');
+fprintf(fid, [repmat('%s\t', 1, size(NamesB,2)-1) '%s\n'], NamesB.');
 fprintf(fid, [repmat('%g\t', 1, size(BranchData,2)-1) '%g\n'], BranchData.');
 fclose(fid);
 
-str = ['results/tree_data_',string,'.txt'];
+str = ['results/treedata_',savename,'.txt'];
 fid = fopen(str, 'wt');
-fprintf(fid, [repmat('%g\t', 1, size(TreeData,2)-1) '%g\n'], TreeData.');
+NamesD(:,2) = TreeData;
+fprintf(fid,'%s\t %g\n',NamesD.');
 fclose(fid);
