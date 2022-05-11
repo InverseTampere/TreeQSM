3baa6f51946ebccd8dd50f7151e245d54721596d
Pasi Raumonen <31093715+QSMtrees@users.noreply.github.com>
2022-05-11 14:24:39 +0300

    Update plot_large_point_cloud.m
    Update

--- src/plotting/plot_large_point_cloud.m
+++ src/plotting/plot_large_point_cloud.m
@@ -1,4 +1,4 @@
-function plot_large_point_clouds(P,fig,ms,rel)
+function plot_large_point_cloud(P,fig,ms,rel)
 
 % Plots a random subset of a large point cloud. The user specifies the
 % relative size of the subset (input "rel" given as in percentage points).
@@ -13,4 +13,4 @@ function plot_large_point_clouds(P,fig,ms,rel)
 rel = 0.5/(1-rel/100); % Compute a coeffiecient
 
 I = logical(round(rel*rand(size(P,1),1)));
-point_cloud_plotting(P(I,:),fig,ms)
+plot_point_cloud(P(I,:),fig,ms)
